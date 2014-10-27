136d4e8e233abc12d92887318aea43641eeebfbd
JanoMinarik <min.jano@gmail.com>
2014-10-27 06:54:02 +0100

    Create grid.h

---
+++ Neon-Calculations/grid.h
@@ -0,0 +1,27 @@
+#ifndef __grid_H_INCLUDED__
+#define __grid_H_INCLUDED__
+#include "neon.h"
+
+class Grid : Neon
+{
+public:
+    int numPoints;
+    int numFnc;
+    void setGrid(int);
+    void unsetGrid();
+    void setCoord();
+    void setCoord(int, double, double, double);
+    void calcGrid();
+    void printGrid();
+
+protected:
+    double *xCoord;
+    double *yCoord;
+    double *zCoord;
+    double **gridValue;
+    int getNumFnc();
+    double getR(int, int);
+    double getValue(int, int);
+};
+
+#endif
