3917ee8847483e8903a6cb9e896f229a67a8b2e1
JanoMinarik <min.jano@gmail.com>
2014-11-06 06:59:09 +0100

    Update main.cpp
    Added scenarios.

--- Neon-Calculations/main.cpp
+++ Neon-Calculations/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <math.h>
+#include <ctime>
 #include "Neon.h"
 #include "Grid.h"
 
@@ -7,16 +7,31 @@ using namespace std;
 
 int main()
 {
-    Neon myNeon;
-    myNeon.printNeon();
+    //Neon myNeon;
+    //myNeon.printNeon();
+    //myNeon.printShell();
 
     std::cout << "Grid:\n";
     Grid myGrid;
+
+    ///first scenario - 2 different points
+    clock_t start = clock();
     myGrid.setGrid(2);
     myGrid.setCoord();
-    //myGrid.setCoord(2, 1.5, 1.5, 1.5);
+    myGrid.setCoord(1, 1.5, 1.5, 1.5);
     myGrid.calcGrid();
+    clock_t finish = clock();
     myGrid.printGrid();
+    cout << "Grid initialization and calculation time: " << (finish-start) << " ns\n";
+
+    ///second scenario - 1 point on the Moon
+    start = clock();
+    myGrid.setCoord(1, 1500, 1500, 1500);
+    myGrid.calcGrid();
+    finish = clock();
+    myGrid.printGrid();
+    cout << "Grid initialization and calculation time: " << (finish-start) << " ns\n";
+
     myGrid.unsetGrid();
 
     return 0;
