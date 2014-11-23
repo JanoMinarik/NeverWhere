46a24cc899e05a66d28815b6137a50fb7c2b2cb5
JanoMinarik <min.jano@gmail.com>
2014-11-23 18:53:12 +0100

    New test scenario with 2 Ne's and 10 Pt's
    Goal:
    To observe decrease of grid values with increasing distance from Ne's.
    Set-up:
    1st Ne is in the center of grid.
    2nd Ne is outside part of grid with most points (8 of 10).
    9th point is located far from center.
    10th point is located very far away.

--- Neon-Calculations/main.cpp
+++ Neon-Calculations/main.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <ctime>
-#include "neon.h"
+//#include "neon.h"
 #include "grid.h"
 
 using namespace std;
 
 int main()
 {
-    /// to test Neon initialization and data
+    /// Neon for testing
     /*Neon myNeon;
     myNeon.printNeon();
     myNeon.printShell();*/
 
     std::cout << "Grid:\n";
     Grid myGrid;
-
     ///first scenario - 2 different Neons in grid
     /// pt 9 - longest distance, pt 10 - on the moon
     clock_t start = clock();
     myGrid.setGrid(2, 10);
     myGrid.setCoord(2);
-    myGrid.setNeon(1, 2, 2, 2);
+    myGrid.setNeon(1, 0, 0, 4);
     myGrid.setCoord(8, 5, 5, 5);
     myGrid.setCoord(9, 1000, 1000, 1000);
     myGrid.calcGrid();
